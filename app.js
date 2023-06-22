6773745fbee29796e34ea0f8bad8bbcac502c98a
Mohammad Ruman <mohammaderuman@gmail.com>
2023-06-23 03:01:37 +0530

    added a database to store the list item mongoDB

--- app.js
+++ app.js
@@ -1,33 +1,81 @@
 const express = require("express");
 const bodyParser = require("body-parser");  
-const date = require(__dirname + "/date.js");
+//date.js
+// const date = require(__dirname + "/date.js");
+
 // console.log(date());
-var items = ["hello", "hi", "bye"];
-let workItems = ["work1", "work2", "work3"];
+const mongoose = require("mongoose");
+
+
 const app = express();
  
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+//mongodb
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+//schema
+const itemsSchema = {
+    name: String
+};
+//model
+const Item = mongoose.model("Item", itemsSchema);
+//mongoose documents
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+//putting all items in an array
+const defaultItems = [item1, item2, item3];
+//inserting all items in the database
+
 app.get("/", (req, res) =>{
  
-  let day = date.getday();
-    res.render("list", {listtittle: day,newlistitems: items});
-    // console.log(day);
+Item.find().then(function(foundItems){
+    if(foundItems.length === 0){
+Item.insertMany(defaultItems).then(function() {
+    console.log("Successfully saved default items to DB.");
+}).catch(function(err) {    
+    console.log(err);
+});
+res.redirect("/");
+    }
+    else{
+        res.render("list", {listtittle: "Today",newlistitems: foundItems});
+    }
+   
+});
+
     
 });
 app.post("/", (req, res) => {
     // console.log(req.body);
-    console.log(req.body.list);
-    var item = req.body.newItem;
-if(req.body.list === "Work"){
-    workItems.push(item);
-    res.redirect("/work");
-}
-   else{
-    items.push(item);
-    res.redirect("/");
-   }
+const itemName = req.body.newItem;
+const item = new Item({
+    name: itemName
+});
+item.save();
+res.redirect("/");
+
+
+
+//for work page
+//     console.log(req.body.list);
+//     var item = req.body.newItem;
+// if(req.body.list === "Work"){
+//     workItems.push(item);
+//     res.redirect("/work");
+// }
+//    else{
+//     items.push(item);
+//     res.redirect("/");
+//    }
   
 });
 
@@ -44,6 +92,15 @@ app.get("/about", (req, res) => {
 //     workItems.push(item);
 //     res.redirect("/work");
 // });
+app.post("/delete", (req, res) => {
+    console.log(req.body);
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId).then(function()   {
+        console.log("Successfully deleted checked item.");
+        res.redirect("/");
+    })
+});
+
 app.listen(3000, () => {    
     console.log("Server running on port 3000");
 });
